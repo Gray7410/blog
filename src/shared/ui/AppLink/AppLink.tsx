ddabe518449c891f37ee882460cf55246de1d4a8
Sergey Fomin <mail@fominso.ru>
2023-02-25 16:58:58 +0500

    eslint

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,30 +1,30 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from "./AppLink.module.scss"
-import {Link, LinkProps} from "react-router-dom";
-import {FC} from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, type LinkProps } from 'react-router-dom';
+import { type FC } from 'react';
+import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
-    PRIMARY = 'primary',
-    SECONDARY = 'secondary',
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
 }
 
-interface AppLinkProps extends LinkProps{
-    className?: string,
-    theme?: AppLinkTheme,
+interface AppLinkProps extends LinkProps {
+  className?: string
+  theme?: AppLinkTheme
 }
 
-export const AppLink:FC<AppLinkProps> = (props) => {
+export const AppLink: FC<AppLinkProps> = (props) => {
     const {
         to,
         className,
         children,
         theme = AppLinkTheme.PRIMARY,
         ...otherProps
-    } = props
+    } = props;
     return (
         <Link
             to={to}
-            className={classNames(cls.AppLink,{},[className, cls[theme]])}
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
             {...otherProps}
         >
             {children}
